6aeb101f03f33d0803e7aacd03b741374b607839
erikgovea <118408955+erikgovea@users.noreply.github.com>
2024-09-08 16:18:39 -0700

    Node_modules
    make node_modules ignore

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your ap</Text>
+      <Text>Open up App.js to start working on your ap!</Text>
       <StatusBar style="auto" />
     </View>
   );
