5ee37c7a3ac51885f09f32a451374478071ad2ec
erikgovea <118408955+erikgovea@users.noreply.github.com>
2024-09-08 23:44:31 -0700

    week 2 implementation so far
    I semi worked on the login, and a drop down menu that gets from the api and puts the type of musclescredit to react website to showing me how to use react native as well as https://www.npmjs.com/package/react-native-element-dropdown for the drop down menu.

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import Home from './Home';
 
-export default function App() {
-  return (
+const Stack =  createNativeStackNavigator();
+
+export default function App(){
+
+return (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+}
+
+function Login({navigation}){
+  return(
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your ap!</Text>
-      <StatusBar style="auto" />
-    </View>
+     
+      <View style={styles.form}>
+        <Text> Username:</Text>
+        <TextInput style={styles.input}></TextInput>
+        <Text >Password: </Text>
+        <TextInput style={styles.input}></TextInput>
+        <Button title='Login' onPress={()=> navigation.navigate('Home')}/>
+      </View>
+</View>
+
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+  flex: 1,
+  backgroundColor: '#fff',
+
+},
+form:{
+  borderRadius:15,
+  backgroundColor:"white"
+},
+input:{
+  padding:2,
+  borderWidth:1,
+}
 });
